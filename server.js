a466c143fded08c385c13c97b39c6a3aea25c636
kinngh <kinngh@me.com>
2021-12-14 17:52:08 +0530

    🧹 Housekeeping.

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const handle = app.getRequestHandler();
 
 //MARK:- MongoDB Connection
 const mongoUrl =
-  process.env.MONGO_URL || "mongodb://localhost:27017/shopify-app";
+  process.env.MONGO_URL || "mongodb://127.0.0.1:27017/shopify-app";
 
 mongoose.connect(
   mongoUrl,
